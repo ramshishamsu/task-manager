241c1527ef1e90abd9284713fb50811de445980c
ramshishamsu <ramshishamsu22@gmail.com>
2025-12-16 20:12:35 +0530

    Frontend - MERN Task Manager

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://task-manager-1mvn.onrender.com/api"
 });
 
 // Automatically attach token to headers
